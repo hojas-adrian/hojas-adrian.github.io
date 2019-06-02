72e1257c95cb3b78ab9d415c93fac64a77aefb32
addrean <34241607+addrean@users.noreply.github.com>
2019-06-02 16:51:00 -0400

    primer update

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function () {
+    var boton = document.querySelector('#button'),
+        body = document.body;
+    boton.addEventListener('click', function () {
+
+        if (body.className != 'menu') {
+            body.classList.add('menu')
+            boton.textContent = 'Close';
+        } else {
+            body.classList.remove('menu')
+            boton.textContent = 'About';
+        }
+    })
+})
